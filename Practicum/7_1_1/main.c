ec9c4ba9f253b9184c44e52eac65e804e3a39b31
Mikhail <44971813+maxfps11@users.noreply.github.com>
2018-11-19 12:10:49 +0300

    Update main.c

--- Practicum/7_1_1/main.c
+++ Practicum/7_1_1/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-unsigned const int max_chars = 10;
-unsigned const int max = 100;
+#define max_chars 10
+#define max 100
 
 struct student{
     char first_name[max_chars];
@@ -13,9 +13,17 @@ struct student{
 int print_list(struct student*, char*);
 
 int main(){
-    struct student stud[max] = {0};
-    char group_number[max_chars] = {0};
-
+    struct student stud[max];
+    char group_number[max_chars];
+    
+    for(int i = 0; i < max; ++i){
+        for(int j = 0; j < max_ch){
+            stud[i].first_name[j] = 0;
+            stud[i].last_name[j] = 0;
+            stud[i].group_number[j] = 0;
+        }
+    }
+    
     for(int i = 0; i < max; ++i){
         scanf("%s%s%s", stud[i].last_name, stud[i].first_name, stud[i].group_number);
         if(!strcmp(stud[i].last_name, "X") && !strcmp(stud[i].first_name, "X") && !strcmp(stud[i].group_number, "X")){
