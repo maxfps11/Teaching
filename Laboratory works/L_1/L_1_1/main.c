b6210c573e772045f8ec7a9898e3b92f353203f6
Mikhail <44971813+maxfps11@users.noreply.github.com>
2018-11-19 11:37:23 +0300

    Update main.c

--- Laboratory works/L_1/L_1_1/main.c
+++ Laboratory works/L_1/L_1_1/main.c
@@ -42,6 +42,7 @@ int validation(struct string* s){
     case OCT:
         for(int i = 0; i < s->length; ++i){
             if (s->str[i] < '0' || s->str[i] > '7'){
+		printf("%d %s\n", s->basis, s->str);
                 printf("%s", msg_err_num);
                 return 1;
             }
@@ -50,6 +51,7 @@ int validation(struct string* s){
     case DEC:
         for(int i = 0; i < s->length; ++i){
             if(s->str[i] < '0' || s->str[i] > '9'){
+		printf("%d %s\n", s->basis, s->str);
                 printf("%s", msg_err_num);
                 return 1;
             }
@@ -59,6 +61,7 @@ int validation(struct string* s){
         for(int i = 0; i < s->length; ++i){
             if(s->str[i] < '0' || (s->str[i] > '9' && s->str[i] < 'A') || s->str[i] > 'F'){
                 if(s->str[i] < '0' || (s->str[i] > '9' && s->str[i] < 'a') || s->str[i] > 'f'){
+		    printf("%d %s\n", s->basis, s->str);
                     printf("%s", msg_err_num);
                     return 1;
                 }
@@ -66,6 +69,7 @@ int validation(struct string* s){
         }
         return s->basis;
     default:
+	printf("%d %s\n", s->basis, s->str);
         printf("%s", msg_err_bas);
         return 1;
     }
